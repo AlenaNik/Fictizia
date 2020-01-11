7fd4339ff362a1b3aac6216c15a8252c9f2c9077
React <react@ZiMacPro-1.local>
2020-01-11 14:07:58 +0100

    children props+

--- src/Counter.js
+++ src/Counter.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react'
+import Button from './Button'
 
 class Counter extends Component {
     state = {
         str: ''
     }
 
-    increment() {
-        this.setState(state => ({
-            str: state.str + 'a'
-        }))
-    }
-    decrement() {
-        this.setState(state => ({
-            
-            str: state.str.substring(0, state.str.length - 1)
-        }))
-    }
+    increment = () => { this.setState(state => ({ str: state.str + 'a '}))
+}
+    decrement = () =>  { this.setState(state => ({ str: state.str.substring(0, state.str.length - 1) })) 
+}
 
     render() {
         return (
            <>
             <div>{this.state.str}</div>
-            <button onClick={() => this.increment()}>Increment</button>
-            <button onClick={() => this.decrement()}>Decrement</button>
+            <Button onClick={this.increment}>Increment</Button>
+            <Button onClick={this.decrement}>Decrement</Button>
            </>
         )
     }
