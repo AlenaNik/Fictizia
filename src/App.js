47055ab539880908889045601629e6e8be724b8d
React <react@ZiMacPro-1.local>
2020-01-18 10:56:51 +0100

    toggle

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React, {Component} from 'react';
 import './App.css';
 import './Users';
-import Users from "./Users";
-import Task from "./Task";
+import axios from 'axios';
+
 
 
 class App extends Component {
     state = {
-        show: false
+        show: false,
+        item: []
     }
 
-    toggleComponent = () => {
+    getData = () => {
+        let url = 'https://jsonplaceholder.typicode.com/todos/1'
+        axios.get(url)
+            .then(res => {
+                const item = res.data;
+                this.setState({ item })
+                this.showComponent()
+            })
+            .catch(err => console.log(err, 'its been an error'))
+    }
+
+    showComponent = () => {
         this.setState({show: !this.state.show})
     }
 
     render() {
         return (
             <>
-                { this.state.show ? <Users/> : '' }
-            <button onClick={this.toggleComponent}> Toggle
-            </button>
-                <Task/>
+                { this.state.show ? <h1>{this.state.item.id}</h1> : '' }
+                <button onClick={this.getData}> Show
+                </button>
             </>
         )
     }
