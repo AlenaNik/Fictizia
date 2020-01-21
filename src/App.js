dc6572f993f07e00070083299f28c206fee81963
AlenaNik <alena.nik.digital@gmail.com>
2020-01-21 21:38:15 +0100

    styling no logic

--- src/App.js
+++ src/App.js
@@ -1,184 +1,123 @@
-import React, {Component} from 'react'
-import './App.css'
-import Select from './Select.component'
+import React, { useState } from 'react'
+// import Select from './Select.component'
+import styled, { createGlobalStyle, css} from 'styled-components'
 
-const countries = [ {
-    id: 1,
-    text: 'Spain'
-},
-{
-    id: 2,
-    text: 'USA'
-},
-]
 
-const province = [ {
-    id: 1,
-    text: 'Guadalajara, Madrid'
-},
-    {
-        id: 2,
-        text: 'Boston, Massachusetts'
-    },
-]
+const GlobalStyle = createGlobalStyle`
+html {
+height: 100%;
+}
+body {
+height: 100%;
+}
+`
+const SharedStyles = css`
+background: #eee;
+height: 40px;
+`
+const StyledFormWrapper = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+padding: 0 20px;
 
-class App extends Component {
-state = {
-    name: '',
-    age: '',
-    surname: '',
-    value: '',
-    isGoing: true,
-    isChecked: true
+`
+const StyledForm = styled.form`
+  width: 100%;
+  max-width: 700px;
+  padding: 40px;
+  background-color: #fff;
+  border-radius: 10px;
+  box-sizing: border-box;
+  box-shadow: 0px 0px 20px 0px rgba(0,0,0,0.2);
+`
+
+
+const StyledFormButton = styled.button`
+display: block;
+padding: 10px 20px;
+background: black;
+border-radius: 5px;
+color: white;
+`
+
+
+const StyledFieldSet = styled.fieldset`
+padding: 10px;
+margin: 20px;
+legend {
+padding: 0 10px;
 }
 
-handleChange = (e) => {
-    const target = e.target
-    const value = target.type === 'checkbox' ? target.checked : target.value
-    const name = target.name
-    this.setState({
-        [name]: value
-    })
-}
-handleSubmit = (e) => {
-    alert('A form was submitted')
-    e.preventDefault()
-}
-    render() {
-        return (
-            <>
-                <form onSubmit={this.handleSubmit} className="px-8 pt-6 pb-8 mb-4">
-                    <label className="text-black text-sm font-bold mb-4">
-                        Name:
-                    <input
-                    className="shadow appearance-none border rounded py-2 px-3 m-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+`
+const StyledInput = styled.input`
+display: block;
+width: 100%;
+${SharedStyles}
+`
+
+const StyledError = styled.fieldset`
+color: red;
+`
+
+const StyledTextArea = styled.textarea`
+background: #eee;
+width: 100%;
+min-height: 100px;
+resize: none;
+${SharedStyles}
+`
+const App = () => {
+    return (
+        <>
+            <GlobalStyle/>
+            <StyledFormWrapper>
+                <StyledForm>
+                    <h2>Contact Form</h2>
+                    <label htmlFor="name">Name</label>
+                    <StyledInput
                     type="text"
                     name="name"
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                    placeholder="Name"
                     />
+                    <label htmlFor="">Email</label>
+                    <StyledInput
+                    type="email"
+                    name="email"
+                    />
+                    <StyledFieldSet>
+                        <legend>
+                            Gender
+                        </legend>
+                        <label>
+                            <input type="radio"
+                                value="female"
+                                name="gender"
+                            />
+                            Female
+                        </label>
+                        <label>
+                            <input type="radio"
+                                   value="male"
+                                   name="gender"
+                            />
+                            Male
+                        </label>
+                    </StyledFieldSet>
+                    <label htmlFor="message">
+                        Message
                     </label>
-                    <label className="text-black text-sm font-bold mb-4">
-                        Surname:
-                        <input
-                            className="shadow appearance-none border rounded py-2 px-3 m-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            type="text"
-                            name="surname"
-                            value={this.state.surname}
-                            onChange={this.handleChange}
-                            placeholder="Surname"
-                        />
-                    </label>
-                    <br />
-                    <label className="text-black text-sm font-bold mb-4">
-                        Age:
-                    <input className="shadow appearance-none border rounded py-2 px-2 m-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="number"
-                        name="age"
-                        value={this.state.age}
-                        onChange={this.handleChange}
-                        placeholder="Age"
-                    />
-                    </label>
-                    <br />
-                    Games:
-                    <input
-                        className="mb-4"
-                        name="Games"
-                        type="checkbox"
-                        checked={this.state.isGoing}
-                        onChange={this.handleChange}
-                    />
-                    <br />
-                    Football:
-                    <input
-                        className="mb-4"
-                        name="Football"
-                        type="checkbox"
-                        checked={this.state.isGoing}
-                        onChange={this.handleChange}
-                    />
-                    <br />
-                    Basketball:
-                    <input
-                        className="mb-4"
-                        name="Basketball"
-                        type="checkbox"
-                        checked={this.state.isGoing}
-                        onChange={this.handleChange}
-                    />
-                    <br />
-                    Art:
-                    <input
-                        className="mb-4"
-                        name="Art"
-                        type="checkbox"
-                        checked={this.state.isGoing}
-                        onChange={this.handleChange}
-                    />
-                    <br />
+                    <StyledTextArea name="message">
+                    </StyledTextArea>
+                    <StyledError>
+                        <p>Error Message Here</p>
+                    </StyledError>
+                    <StyledFormButton type="submit">Send Message</StyledFormButton>
+                </StyledForm>
 
-                    <textarea
-                        className="bg-gray-200"
-                        placeholder="description"
-                        value={this.state.value} onChange={this.handleChange} />
-                    <br />
-
-
-                   <div className="radio">
-                    <label>
-                        <input type="radio" value="option1"
-                               checked={this.state.isChecked}
-                               onChange={this.handleChange}
-                        />
-                        Option 1
-                    </label>
-                </div>
-                <div className="radio">
-                    <label>
-                        <input type="radio" value="option2"
-                               checked={!this.state.isChecked}
-                               onChange={this.handleChange}
-                        />
-                        Option 2
-                    </label>
-                </div>
-                <div className="radio">
-                    <label>
-                        <input type="radio" value="option3"
-                               checked={!this.state.isChecked}
-                               onChange={this.handleChange}
-                        />
-                        Option 3
-                    </label>
-                </div>
-                <br/>
-
-                    <Select
-                        items={countries}
-                        onChange={(e) => console.log(e.target.value)}
-                    />
-                    <Select
-                        items={province}
-                        onChange={(e) => console.log(e.target.value)}
-                    />
-                    <br />
-                    <input
-                        className="bg-black hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="submit"
-                        value="Submit"/>
-
-                </form>
-
-               {/*  <Select */}
-               {/*items={veggies}*/}
-               {/*onChange={(e) => console.log(e.target.value)}*/}
-               {/*/>*/}
-            </>
-        )
-    }
+            </StyledFormWrapper>
+        </>
+    )
 
 }
-
 export default App;
