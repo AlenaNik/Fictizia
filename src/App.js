c0b2080a61bf23c5cacc01e0298d68b644e18796
AlenaNik <alena.nik.digital@gmail.com>
2020-01-25 20:57:26 +0100

    after coupon

--- src/App.js
+++ src/App.js
@@ -1,201 +1,99 @@
 import React, {Component} from 'react'
 import './App.css'
-import Select from './Select.component'
 
-const countries = [ {
-    id: 1,
-    text: 'Spain'
-},
-{
-    id: 2,
-    text: 'USA'
-},
-]
 
-const province = [ {
-    id: 1,
-    text: 'Guadalajara, Madrid'
-},
+const products = [
+    {
+        id: 1,
+        name: "hat",
+        price: 200
+    },
     {
         id: 2,
-        text: 'Boston, Massachusetts'
+        name: "skirt",
+        price: 400
     },
+    {
+        id: 3,
+        name: "pants",
+        price: 800
+    }
 ]
-
 class App extends Component {
 state = {
-    name: '',
-    age: '',
-    surname: '',
-    value: '',
-    message: '',
-    checked: false,
-    gender: '',
-    error: '',
+    productsInCart: [],
+    coupon: '',
+    isValid: true
 }
 
-handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
+addProduct = (product) => {
+    const products = [...this.state.productsInCart, product];
     this.setState({
-        [name]: value,
-        gender: e.target.value,
-        message: e.target.value,
-        checked: !this.state.checked,
+        productsInCart: products
     })
 }
 
-
-handleSubmit = (e) => {
-    e.preventDefault()
-    alert('A form was submitted')
+handleRemove = (product) => {
+    let productsToRemove = this.state.productsInCart.filter(p => p !== product)
+    this.setState({
+        productsInCart: productsToRemove
+    })
 }
+
+calculateTotalPrice = () => {
+    let total = this.state.productsInCart.reduce((acc, curr) => acc + curr.price, 0);
+    if (this.state.isValid) {
+        total = total * 0.9
+    }
+    return total
+}
+
+handleInputChange = (e) => {
+    this.setState({
+        coupon: e.target.value
+    })
+}
+    verifyCoupon = () => {
+    if (this.state.coupon === "SAVE10") {
+        this.setState({
+            isValid: true })
+    } else {
+        alert("Something went wrong!")
+    }
+    }
 
     render() {
         return (
             <>
-                <form onSubmit={this.handleSubmit} className="px-8 pt-6 pb-8 mb-4">
-                    <label className="text-black text-sm font-bold mb-4">
-                        Name:
-                    <input
-                    className="shadow appearance-none border rounded py-2 px-3 m-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    type="text"
-                    name="name"
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                    placeholder="Name"
-                    />
-                    </label>
-                    <label className="text-black text-sm font-bold mb-4">
-                        Surname:
-                        <input
-                            className="shadow appearance-none border rounded py-2 px-3 m-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                            type="text"
-                            name="surname"
-                            value={this.state.surname}
-                            onChange={this.handleChange}
-                            placeholder="Surname"
-                        />
-                    </label>
-                    <br />
-                    <label className="text-black text-sm font-bold mb-4">
-                        Age:
-                    <input className="shadow appearance-none border rounded py-2 px-1 m-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                        type="number"
-                        name="age"
-                        value={this.state.age}
-                        onChange={this.handleChange}
-                        placeholder="Age"
-                    />
-                    </label>
-                    <br />
-                    Games:
-                    <input
-                        className="mb-4"
-                        name="Games"
-                        type="checkbox"
-                        onChange={this.handleChange}
-                        defaultChecked={this.state.checked}
-                    />
-                    <br />
-                    Football:
-                    <input
-                        className="mb-4"
-                        name="Football"
-                        type="checkbox"
-                        onChange={this.handleChange}
-                        defaultChecked={this.state.checked}
-                    />
-                    <br />
-                    Basketball:
-                    <input
-                        className="mb-4"
-                        name="Basketball"
-                        type="checkbox"
-                        onChange={this.handleChange}
-                        defaultChecked={this.state.checked}
-                    />
-                    <br />
-                    Art:
-                    <input
-                        className="mb-4"
-                        name="Art"
-                        type="checkbox"
-                        onChange={this.handleChange}
-                        defaultChecked={this.state.checked}
-                    />
-                    <br />
-
-                    <textarea
-                        name="textfield"
-                        value={this.state.message}
-                        onChange={this.handleChange}
-                        className="bg-gray-200"
-                        placeholder="description"
-                    />
-                    <br />
-
-
-                   <div className="radio">
-                       <legend>Gender</legend>
-                    <label>
-                        <input type="radio"
-                               value="female"
-                               defaultChecked={true}
-                               checked={this.state.gender === 'female'}
-                               onChange={this.handleChange}
-                        />
-                        Female
-                    </label>
-                </div>
-                <div className="radio">
-                    <label>
-                        <input type="radio"
-                               value="male"
-                               checked={this.state.gender === 'male'}
-                               onChange={this.handleChange}
-                        />
-                        Male
-                    </label>
-                </div>
-                    <div className="radio">
-                        <label>
-                            <input type="radio"
-                                   value="neutral"
-                                   checked={this.state.gender === 'neutral'}
-                                   onChange={this.handleChange}
-
-                            />
-                            Gender-neutral
-                        </label>
+                {products.map(product =>
+                    <div>
+                        <h1>{product.name}</h1>
+                        <p>{product.price}$</p>
+                        <button onClick={() => this.addProduct(product)}>Add to card</button>
                     </div>
-                <div className="radio">
-                    <label>
-                        <input type="radio"
-                               value="notprovided"
-                               checked={this.state.gender === 'no-provided'}
-                               onChange={this.handleChange}
-                        />
-                        Prefer not to say
-                    </label>
-                </div>
+                )}
+
+                <hr />
+                Card: <span>Total {this.calculateTotalPrice()}$</span>
+                <ul>
+                {this.state.productsInCart.map((productInCard, key) =>
+                    <li key={key}>
+                        <h1>{productInCard.name}</h1>
+                        <p>{productInCard.price}$</p>
+                        <button onClick={() => this.handleRemove(productInCard)}>Remove</button>
+                    </li>
+                )}
+                </ul>
+                <br/>
+                Enter the cupon code:
+                <input type="text"
+                       placeholder="type here"
+                       value={this.state.coupon}
+                       onChange={this.handleInputChange}
+                />
+                <button onClick={this.verifyCoupon}>Verify coupon</button>
                 <br/>
 
-                    <Select
-                        items={countries}
-                        onChange={(e) => console.log(e.target.value)}
-                    />
-                    <Select
-                        items={province}
-                        onChange={(e) => console.log(e.target.value)}
-                    />
-                    <br />
-                    <input
-                        className="bg-black hover:bg-gray-700 text-gray-100 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                        type="submit"
-                        value="Submit"/>
-
-                </form>
             </>
         )
     }
